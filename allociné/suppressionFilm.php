cd72cc7527c942c972d159094e83c2037a56b97d
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-05-18 14:38:29 +0200

    allociné

--- allociné/suppressionFilm.php
+++ allociné/suppressionFilm.php
@@ -1,21 +1,16 @@
 <?php
-$ret = "";
+
 if(isset($_GET["titre"]))
 {
     //On ouvre en lecture
     $tableauJson = 'tableau.json';
     $current = file_get_contents($tableauJson);
-    $numL = 0;
     $GET = [];
-    while($GET=json_decode($current))
-    {
-        if($numL!=$_GET["titre"])
-        {
-            //On stocke la ligne dans un tableau sauf celle qu'on veut supprimer
-            array_push ($GET, $_GET);
-        }
-        $numL++;
-    }
+   
+    function suppression() 
+{
+    unset($GET['titre']);
+}
 
     $current=json_encode($GET);
     file_put_contents($tableauJson,$current);
@@ -24,22 +19,4 @@ if(isset($_GET["titre"]))
 
 
   
-    //On ouvre le fichier en écriture de remplacement
-    $tableauJson = 'tableau.json';
-    $current = file_get_contents($tableauJson);
-    $tableau = json_decode($current,true);
-
-    for ($i=0 ; $i<count($tableau);++$i)
-    {
-        if($ret=="")
-        {
-            //on a eut aucune erreur
-            $ret = "J'ai bien supprimée";
-        }
-    }
-  
-    
-    
-
-
 ?>
